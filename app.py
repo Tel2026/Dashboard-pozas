3f1577059f4902aa3361384283f0e94a01c7d9f0
Tel2026 <telemetrianewmont@gmail.com>
2025-07-10 14:34:54 -0500

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import json
 import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,5 +17,7 @@ def get_poza(nombre):
         data = json.load(f)
     bombas = data.get(nombre, [])
     return jsonify(bombas)
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
